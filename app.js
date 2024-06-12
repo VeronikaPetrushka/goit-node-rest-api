942fa85fbbc05595e50e0f61302e54a298434dfa
Veronika <veronikapetrushka@gmail.com>
2024-06-12 21:04:48 +0200

    express static

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
 
-app.use("/avatars", express.static(path.resolve("tmp")));
+app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 app.use("/api/users", userRouter);
